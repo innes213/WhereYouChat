e0acd677d0a218deed851e38367b63f3de171c99
Rob Innes Hislop <rob.hislop@rd.io>
2015-05-17 00:31:01 -0700

    Working login
    TODO: Probably want to create new Parse application and client ID

--- WhereYouChat?/LoginViewController.swift
+++ WhereYouChat?/LoginViewController.swift
@@ -10,26 +10,70 @@ import UIKit
 
 class LoginViewController: UIViewController {
 
+    
+    @IBOutlet weak var emailLabel: UITextField!
+    @IBOutlet weak var passwordLabel: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         // Do any additional setup after loading the view.
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
     
-
+    
+    @IBAction func onSignIn(sender: AnyObject) {
+        
+        PFUser.logInWithUsernameInBackground(emailLabel.text, password:passwordLabel.text) {
+            (user: PFUser?, error: NSError?) -> Void in
+            if user != nil {
+                NSOperationQueue.mainQueue().addOperationWithBlock({ () -> Void in
+                    self.performSegueWithIdentifier("loginToMapSegue", sender: self)
+                })
+            } else {
+                // The login failed. Check error to see why.
+                let alert = UIAlertView(title: "Error", message: error?.description, delegate: nil, cancelButtonTitle: "Ok")
+                alert.show()
+            }
+        }
+        
+    }
+    
+    @IBAction func onSignUp(sender: AnyObject) {
+        var user = PFUser()
+        user.username = emailLabel.text
+        user.password = passwordLabel.text
+        user.email = user.username
+        // other fields can be set just like with PFObject
+        user["phone"] = "415-392-0202"
+        
+        user.signUpInBackgroundWithBlock {
+            (succeeded: Bool, error: NSError?) -> Void in
+            if let error = error {
+                let errorString = error.userInfo?["error"] as? NSString
+                // Show the errorString somewhere and let the user try again.
+                let alert = UIAlertView(title: "Error", message: errorString! as String, delegate: nil, cancelButtonTitle: "Ok")
+                alert.show()
+                
+            } else {
+                NSOperationQueue.mainQueue().addOperationWithBlock({ () -> Void in
+                    self.performSegueWithIdentifier("loginToMapSegue", sender: self)
+                })
+            }
+        }
+    }
     /*
     // MARK: - Navigation
-
+    
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    // Get the new view controller using segue.destinationViewController.
+    // Pass the selected object to the new view controller.
     }
     */
-
+    
 }
---
+++ WhereYouChat?/MessageCell.swift
@@ -0,0 +1,34 @@
+//
+//  MessageCell.swift
+//  WhereYouChat?
+//
+//  Created by Rob Hislop on 5/16/15.
+//  Copyright (c) 2015 Swift Team Six. All rights reserved.
+//
+
+import UIKit
+
+class MessageCell: UITableViewCell {
+    
+    @IBOutlet weak var messageLabel: UILabel!
+    
+    var message: String? {
+        didSet {
+            if let message = message {
+                messageLabel.text = message
+            }
+        }
+    }
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+    
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+        
+        // Configure the view for the selected state
+    }
+    
+}
